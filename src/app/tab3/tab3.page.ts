f0b0a494719fc2bf99029a4161b844ed9c073bbc
Bilkiss Dulloo <bilkiss.dulloo@gmail.com>
2019-02-05 10:39:18 +0400

    Updated tab3 component - "Mes favoris". Display list of my favorites cats from localstorage, order by score. Added heart icon to the right of image for the 1st, 2nd & 3rd.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,8 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { EndpointService } from '../services/endpoint.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page implements OnInit {
+
+  votesArray: any = [];
+  catVotedList: any = [];
+  catListObj:any;
+  catList:any;
+
+  constructor(private endpointService: EndpointService){
+
+  }
+
+  ngOnInit(){
+
+    this.votesArray = localStorage.getItem("votesArray");
+    this.votesArray = (this.votesArray) ? JSON.parse(this.votesArray) : [];
+    console.log("votesArray click vote: ", this.votesArray);
+
+    this.getListCat();
+
+  }
+
+  getListCat(){
+    this.endpointService.getCats().subscribe( res => {
+      this.catListObj = res;
+      this.catList = this.catListObj.images;
+      console.log("catList: ", this.catList);
+
+      this.catVotedList = this.catList.filter( x => this.votesArray.includes(x.id));
+
+      console.log("catVotedList: ", this.catVotedList);
+
+
+    }, error => {
+      console.log("Error: ", error);
+    })
+  }
+
+}
